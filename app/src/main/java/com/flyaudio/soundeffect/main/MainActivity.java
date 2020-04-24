6927c6a2d884a8475e82e523cb50a3f73014007f
wangdongping <wangdongping@flyaudio.cn>
2020-04-24 13:03:36 +0800

    [dev]初始化项目结构

---
+++ app/src/main/java/com/flyaudio/soundeffect/main/MainActivity.java
@@ -0,0 +1,21 @@
+package com.flyaudio.soundeffect.main;
+
+import com.flyaudio.lib.base.BaseActivity;
+
+/**
+ * @author Dongping Wang
+ * @date 20-4-24
+ * email wangdongping@flyaudio.cn
+ */
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected int getLayoutId() {
+        return 0;
+    }
+
+    @Override
+    protected void init() {
+
+    }
+}
