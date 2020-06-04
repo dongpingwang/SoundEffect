12a14f90ead14741ae62305c159a4381eb694ada
Dongping Wang <927718579@qq.com>
2020-06-04 22:54:36 +0800

    [dev]主界面ViewPager改为FrameLayout

--- app/src/main/java/com/flyaudio/soundeffect/main/activity/MainActivity.java
+++ app/src/main/java/com/flyaudio/soundeffect/main/activity/MainActivity.java
@@ -1,29 +1,29 @@
 package com.flyaudio.soundeffect.main.activity;
 
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 
 import com.flyaudio.lib.base.BaseActivity;
 import com.flyaudio.lib.ui.flyaudio.FlyTabBar;
 import com.flyaudio.soundeffect.R;
 import com.flyaudio.soundeffect.backup.logic.UsbManager;
-import com.flyaudio.soundeffect.comm.view.NoScrollViewPager;
 import com.flyaudio.soundeffect.equalizer.fragment.EqFragment;
-import com.flyaudio.soundeffect.main.adpater.MainPagerAdapter;
 import com.flyaudio.soundeffect.position.fragment.ListenPositionFragment;
 import com.flyaudio.soundeffect.setting.fragment.SettingFragment;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * @author Dongping Wang
  * @date 20-4-24
  * email wangdongping@flyaudio.cn
  */
-public class MainActivity extends BaseActivity {
+public class MainActivity extends BaseActivity implements FlyTabBar.OnSelectedIndexChangedListener {
+
+    private static final int FRAGMENT_EQ = 0;
+    private static final int FRAGMENT_POSITION = 1;
+    private static final int FRAGMENT_SETTING = 2;
 
     private FlyTabBar tabBar;
-    private NoScrollViewPager pagerMain;
+    private Fragment[] fragments = new Fragment[FRAGMENT_SETTING + 1];
 
     @Override
     protected int getLayoutId() {
@@ -33,30 +33,35 @@ public class MainActivity extends BaseActivity {
     @Override
     protected void init() {
         initTabs();
-        initViewPages();
         UsbManager.getInstance().init();
     }
 
     private void initTabs() {
         tabBar = getView(R.id.tab_bar);
-        tabBar.setOnSelectedIndexChangedListener(new FlyTabBar.OnSelectedIndexChangedListener() {
-            @Override
-            public void onSelectedIndexChanged(int pre, int current) {
-                pagerMain.setCurrentItem(current, false);
-            }
-        });
+        tabBar.setOnSelectedIndexChangedListener(this);
+        tabBar.setSelectedIndex(FRAGMENT_EQ);
     }
 
-    private void initViewPages() {
-        pagerMain = getView(R.id.vp_main);
-        List<Fragment> fragments = new ArrayList<>();
-        fragments.add(new EqFragment());
-        fragments.add(new ListenPositionFragment());
-        fragments.add(new SettingFragment());
-        MainPagerAdapter adapter = new MainPagerAdapter(getSupportFragmentManager(), fragments);
-        pagerMain.setAdapter(adapter);
-        tabBar.setSelectedIndex(0);
+    @Override
+    public void onSelectedIndexChanged(int pre, int current) {
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        if (fragments[current] == null) {
+            if (current == FRAGMENT_EQ) {
+                fragments[current] = new EqFragment();
+            } else if (current == FRAGMENT_POSITION) {
+                fragments[current] = new ListenPositionFragment();
+            } else {
+                fragments[current] = new SettingFragment();
+            }
+            ft.add(R.id.fl_main, fragments[current]);
+        }
+        if (pre >= 0) {
+            ft.hide(fragments[pre]);
+        }
+        ft.show(fragments[current]);
+        ft.commit();
     }
+
 
     @Override
     protected void onDestroy() {
